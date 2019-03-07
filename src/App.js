02b2065864792e7f506977ccfaa6567e1b78fe75
Katsuki <Katsuki_Sato0130@icloud.com>
2019-03-07 22:06:32 +0900

    Add basic structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+
+const GET_POSTS = `
+  {
+    posts {
+        id
+        title
+        description
+    }
+  }
+`;
+
+const baseUrl = 'http://localhost:3000/graphql';
 
 class App extends Component {
+  componentDidMount() {
+    this.fetchPosts();
+  }
+
+  fetchPosts = () => {
+    axios({
+      url: baseUrl,
+      method: 'post',
+      data: { query: GET_POSTS}
+    })
+    .then((result) => {
+      console.log(result.data)
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h2>GraphQL Front</h2>
+
       </div>
     );
   }
